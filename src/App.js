e0a31145c3994b379f77413c1f60fd03af357343
djadudwns12 <djadudwns12@naver.com>
2024-11-04 12:00:51 +0900

    feat : Header 컴포넌트 생성및 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './component/Header';
+('./component/Header');
 
 function App() {
   return (
     <div className="App">
       <h2>ToDoList</h2>
-      <div>Header</div>
+      <div>
+        <Header />
+      </div>
       <div>To Do Editor</div>
       <div>To Do List</div>
     </div>
