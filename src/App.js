7c9ba411b66fed0105bae62bf3862d60613822bb
djadudwns12 <djadudwns12@naver.com>
2024-11-04 14:20:47 +0900

    feat : 기능구현을 위해 목업데이터를 선정 제공했다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
+//  mockup데이터를 널어주기
+const mokTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: '리액트 공부하기',
+    createdDate: new Date().getDate(),
+  },
+  {
+    id: 1,
+    isDone: true,
+    content: '빨래하기',
+    createdDate: new Date().getDate(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '청소하기',
+    createdDate: new Date().getDate(),
+  },
+];
 
 function App() {
+  // 할일 아이템의 상태를 관리할 state 생성
+  const [todo, setTodo] = useState(mokTodo);
+
   return (
     <div className="App">
       <h2>ToDoList</h2>
