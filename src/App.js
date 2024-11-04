d8cddeaece61874d325fa306de4ecc426b509c05
djadudwns12 <djadudwns12@naver.com>
2024-11-04 11:49:50 +0900

    feat : 기본레이아웃 + css 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>ToDoList</h2>
+      <div>Header</div>
+      <div>To Do Editor</div>
+      <div>To Do List</div>
+    </div>
+  );
 }
 
 export default App;
