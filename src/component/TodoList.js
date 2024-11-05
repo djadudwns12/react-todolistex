6b8740b5d8cc1b4c41b350f78baacfaa111d5af4
djadudwns12 <djadudwns12@naver.com>
2024-11-05 11:39:13 +0900

    feat : 검색어를 입력하면 searchState에 할당되도록 함

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.css';
 const TodoList = ({ todo }) => {
   // 구조분해할당
+  const [search, setSearch] = useState('');
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
@@ -9,6 +15,8 @@ const TodoList = ({ todo }) => {
         className="searchBar"
         placeholder="검색어를 입력하세요..."
         type="text"
+        onChange={onChangeSearch}
+        value={search}
       />
       <div className="list_wrapper">
         {todo.map((item) => (
